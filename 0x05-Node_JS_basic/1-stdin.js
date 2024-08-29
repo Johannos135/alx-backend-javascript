9a8944eb123197b9d630624dd9f61d3573f58cf2
Johanne Essiere <essierejohanne1@gmail.com>
2024-08-29 15:51:08 +0000

    1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,19 @@
+/**
+ * program executed through cli
+ * - it should display welcome message
+ * - allow user to input their name
+ * - display user name
+ * - message when closing
+ */
+
+const {stdout, stdin} = process;
+
+stdout.write('Welcome to Holberton School, what is your name?\n');
+stdin.on('data', (data) => {
+  stdout.write(`Your name is: ${data}`);
+  process.exit();
+});
+
+process.on('exit', () => {
+  stdout.write('This important software is now closing\n');
+});
