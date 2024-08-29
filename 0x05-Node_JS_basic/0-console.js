b06959c6bfcd1d5f1180acbcedc70531ea5236ce
Johanne Essiere <essierejohanne1@gmail.com>
2024-08-29 15:31:29 +0000

    0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ *  prints in STDOUT the string argument
+ *  param {string} message
+ */
+function displayMessage(message) {
+ process.stdout.write(message);
+}
+
+module.exports = displayMessage;
